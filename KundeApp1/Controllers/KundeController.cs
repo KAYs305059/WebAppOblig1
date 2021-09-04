2cd7c69e4e83658b6c89f8530a641ecaed58ddf1
Khadar Ahmed Yusuf <Khadar_yo@hotmail.com>
2021-09-04 13:53:24 +0200

    adding all files

---
+++ KundeApp1/Controllers/KundeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using KundeApp1.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KundeApp1.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class KundeController : ControllerBase
+    {
+        public List<Kunde> HentAlle()
+        {
+            var kundene = new List<Kunde>();
+
+            var kunde1 = new Kunde();
+            kunde1.navn = "Kristian Bakke";
+            kunde1.adresse = "Bjornveien 12";
+
+            var kunde2 = new Kunde
+            {
+                navn = "Seline Hansen",
+                adresse = "Nyjordet 31"
+            };
+
+            kundene.Add(kunde1);
+            kundene.Add(kunde2);
+
+            return kundene;
+        }
+    }
+}
---
+++ KundeApp1/Model/KundeDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace KundeApp1.Model
+{
+    public class KundeDB :DbContext
+    {
+        public KundeDB (DbContextOptions<KundeDB> options) : base(options)
+        {
+            //Forsikrer oss om at databasen blir laget.
+            Database.EnsureCreated();
+        }
+
+        //Kobler oss til databasen
+        public DbSet<Kunde> Kunder { get; set; }
+    }
+}
